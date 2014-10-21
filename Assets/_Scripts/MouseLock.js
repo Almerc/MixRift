10f14808567022d30992074a533d9858023b859b
Alex Watkinson <alex.watkinson@nokia.com>
2014-10-21 23:01:32 +0100

    Add MouseLock script

---
+++ Assets/_Scripts/MouseLock.js
@@ -0,0 +1,11 @@
+﻿
+	function Update () {
+		if (Input.GetMouseButtonDown (0)){
+			Screen.lockCursor = true;
+			}
+	
+		if (Input.GetKeyDown ("escape")){
+			Screen.lockCursor = false;
+			}
+
+	}
